098edda1c4004ddfd3a766fd7a50c88400cf5c7b
lishuo1607 <lishuo1607@163.com>
2017-09-14 20:12:40 +0800

    [add] new add some function and my arm is hurt

--- ls_underscore.js
+++ ls_underscore.js
@@ -53,4 +53,80 @@
     _.VERSION = '1.8.3'
 
 
+    // 用于内部使用的高阶函数, 传入回调函数
+    // 主要用来执行函数病改变所执行函数的作用域, 最后加了一个argCount参数来制定参数个数
+    var optimizeCb = function(func, context, argCount) {
+        if (context === void 0) return func;
+        switch (argCount == null ? 3 : argCount) {
+            case 1: return function(value) {
+                return func.call(context, value);
+            };
+            case 2: return function(value, other) {
+                return func.call(context, value, other);
+            };
+            case 3: return function(value, index, collection) {
+                return func.call(context, value, index, collection);
+            };
+            case 4: return function(accumulator, value, index, collection) {
+                return func.call(context, accumulator, value, index, collection);
+            };
+        }
+        return function(){
+            return func.apply(context, arguments);
+        };
+    };
+
+
+    var cb = function(value, context, argCount) {
+        if (value == null) return _.identity;
+        if (_.isFunction(value)) return optimizeCb(value, context, argCount);
+        if (_.isObject(value)) return _.matcher(value);
+        return _.property(value);
+    };
+    _.iteratee = function(value, context) {
+        return cb(value, context, Infinity);
+    };
+
+    // 内部函数用于创建分配器函数
+    var createAssigner = function(keysFunc, undefineOnly) {
+        return function(obj) {
+            var length = arguments.length;
+            if (length < 2 || obj == null) return obj;
+            for (var index = 1; index < length; index++) {
+                var source = arguments[index],
+                    keys = keysFunc(source),
+                    l = keys.length;
+                for (var i = 0; i < l; i++) {
+                    var key = keys[i];
+                    if (!undefinedOnly || obj[key] == void 0) obj[key] = source[key];
+                }
+            }
+            return obj;
+        }
+    }
+
+    // 内部函数用于创建一个新的对象, 这个对象继承自另外一个???
+    var baseCreate = function(prototype) {
+        if (!_.isObject(prototype)) return {};
+        if (nativeCreate) return nativeCreate(prototype);
+        Ctor.prototype = prototype;
+        var result = new Ctor;
+        Ctor.prototype = null;
+        return result;
+    };
+
+    var property = function(key) {
+        return function(obj) {
+            return obj == null ? void 0 : obj[key];
+        };
+    };
+
+    // 
+    var MAX_ARRAY_INDEX = Math.pow(2, 53) - 1;
+    var getLength = property('length');
+    var isArrayLike = function(collection) {
+        var length = getLength(collection);
+        return typeof length == 'number' && length >= 0 && length <= MAX_ARRAY_INDEX;
+    };
+
 }.call(this));
