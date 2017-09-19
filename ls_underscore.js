24e764bb21c891dc14605afac3557900fdac40da
nlls1990 <124639013@qq.com>
2017-09-19 14:43:31 +0800

    [feature] Add map collection.

--- ls_underscore.js
+++ ls_underscore.js
@@ -145,4 +145,17 @@
         return obj;
     };
 
+    _.map = _.collect = function(obj, iteratee, context) {
+        iteratee = cb(iteratee, context);
+        var keys = !isArrayLike(obj) && _.keys(obj),
+            length = (keys || obj).length,
+            results = Array(length);
+
+        for (var index = 0; index < length; index++) {
+            var currentKey = keys ? keys[index] : index;
+            results[index] = iteratee(obj[currentKey], currentKey, obj);
+        }
+        return results;
+    };
+
 }.call(this));
