f3ce9068f3ee135259e58bff146174fb58e86ed2
nlls1990 <124639013@qq.com>
2017-09-21 20:20:05 +0800

    [feature] Add contains function

--- ls_underscore.js
+++ ls_underscore.js
@@ -215,13 +215,30 @@
 
         _.every = _.all = function(obj, predicate, context) {
             predicate = cb(predicate, context);
-            var keys = !isArrayLike(obj) && _,keys(obj),
+            var keys = !isArrayLike(obj) && _.keys(obj),
                 length = (keys || obj).length;
             for (var index = 0; index < length; index++) {
                 var currentKey = keys ? keys[index] : index;
                 if (!predicate(obj[currentKey], currentKey, obj)) return false;
             }
             return true;
+        };
+
+        _.some = _.any = function(obj, predicate, context) {
+            predicate = cb(predicate, context);
+            var keys = !isArrayLike(obj) && _.keys(obj),
+                length = (keys || obj).length;
+            for (var index = 0; index < length; index++) {
+                var currentKey = keys ? keys[index] : index;
+                if (predicate(obj[currentKey], currentKey, obj)) return true;
+            }
+            return false;
+        };
+
+        _.contains = _.includes = _.include = function(obj, item, fromIndex, guard) {
+            if (!isArrayLike(obj)) obj = _.values(obj);
+            if (typeof fromIndex != 'number' || guard) fromIndex = 0;
+            return _.indexOf(obj, item, fromIndex) >= 0;
         };
 
     
