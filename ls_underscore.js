3f254041bcca4edb20dd6430ca67130c8c318904
nlls1990 <124639013@qq.com>
2017-09-21 13:22:16 +0800

    [comment] Add all match truth test

--- ls_underscore.js
+++ ls_underscore.js
@@ -213,6 +213,17 @@
             return _.filter(obj, _.negate(cb(predicate)), context);
         };
 
+        _.every = _.all = function(obj, predicate, context) {
+            predicate = cb(predicate, context);
+            var keys = !isArrayLike(obj) && _,keys(obj),
+                length = (keys || obj).length;
+            for (var index = 0; index < length; index++) {
+                var currentKey = keys ? keys[index] : index;
+                if (!predicate(obj[currentKey], currentKey, obj)) return false;
+            }
+            return true;
+        };
+
     
 
 }.call(this));
