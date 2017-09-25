d8675bbf5e4cd045afc97d07cdcf3c623ff8ea8a
nlls1990 <124639013@qq.com>
2017-09-25 21:43:24 +0800

    [Add] add max and findwhere and sublime 3 project file

--- ls_underscore.js
+++ ls_underscore.js
@@ -254,4 +254,40 @@
         });
     };
 
+    _.pluck = function(obj, key) {
+        return _.map(obj, _.property(key));
+    };
+
+    _.where = function(obj, attrs) {
+        return _.filter(obj, _.matcher(attrs));
+    };
+
+    _.findWhere = function(obj, attrs) {
+        return _.find(obj, _.matcher(attrs));
+    };
+
+    _.max = function(obj, iteratee, context) {
+        var result = -Infinity, lastComputed = -Infinity,
+            value, computed;
+        if (iteratee == null && obj != null) {
+            obj = isArrayLike(obj) ? obj : _.values(obj);
+            for (var i = 0, length = obj.length; i < length; i++) {
+                value = obj[i];
+                if (value > result) {
+                    result = value;
+                }
+            }
+        } else {
+            iteratee = cb(iteratee, context);
+            _.each(obj, function(value, index, list) {
+                computed = iteratee(value, index, list);
+                if (computed > lastComputed || computed === -Infinity && result === -Infinity) {
+                    result = value;
+                    lastComputed = computed;
+                }
+            });
+        }
+        return result;
+    };
+
 }.call(this));
