4c055b0940c132c088d2ac46538da9583e065034
lishuo <124639013@qq.com>
2017-09-29 22:05:34 +0800

    [underscore] 添加随机数组的生成函数.

--- ls_underscore.js
+++ ls_underscore.js
@@ -314,4 +314,19 @@
         return result;
     };
 
+    // 生成一个随机数的集合, 使用最新版本的Fisher-Yates shuffle
+    _.shuffle = function(obj) {
+        var set = isArrayLike(obj) ? obj : _.values(obj);
+        var length = set.length;
+        var shuffled = Array(length);
+        for (var index = 0, rand; index < length; index++) {
+            rand = _.random(0, index);
+            if (rand !== index) shuffled[index] = shuffled[rand];
+            shuffled[rand] = set[index];
+        }
+        return shuffled;
+    };
+
+    
+
 }.call(this));
