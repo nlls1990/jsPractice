4abba6829397fbb9e37bdc8906b1b310b482e4f8
lishuo <124639013@qq.com>
2017-09-29 21:54:14 +0800

    [min] Add minimum element

--- ls_underscore.js
+++ ls_underscore.js
@@ -290,4 +290,28 @@
         return result;
     };
 
+    _.min = function(obj, iteratee, context) {
+        var result = Infinity, lastComputed = Infinity,
+            value, computed;
+        if (iteratee == null && obj != null) {
+            obj = isArrayLike(obj) ? obj : _.values(obj);
+            for (var i = 0, length = obj.length; i < length; i++) {
+                value = obj[i];
+                if (value < result) {
+                    result = value;
+                }
+            }
+        } else {
+            iteratee =cb(iteratee, context);
+            _.each(obj, function(value, index, list) {
+                computed = iteratee(value, index, list);
+                if (computed < lastComputed || computed === Infinity && result === Infinity) {
+                    result = value;
+                    lastComputed = computed;
+                }
+            });
+        }
+        return result;
+    };
+
 }.call(this));
