76072645d9f075788d5b581f8590cbb22ecd9653
lishuo <124639013@qq.com>
2017-10-11 19:41:59 +0800

    Add undefined type

--- js_book/demo_2_1.js
+++ js_book/demo_2_1.js
@@ -1,4 +1,8 @@
-var message = "some string";
-alert(typeof message);
-alert(typeof(message));
-alert(typeof 95);
+// var message = "some string";
+// alert(typeof message);
+// alert(typeof(message));
+// alert(typeof 95);
+
+var message;
+alert(message == undefined); 
+alert(typeof message)
