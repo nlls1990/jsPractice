56219f1256cd83d8857edf35392ea4f4dc79a9d3
lishuo <124639013@qq.com>
2017-10-13 12:57:57 +0800

    [object] try object

--- js_book/demo_2_1.js
+++ js_book/demo_2_1.js
@@ -3,6 +3,16 @@
 // alert(typeof(message));
 // alert(typeof 95);
 
-var message;
-alert(message == undefined); 
-alert(typeof message)
+// var message;
+// alert(message == undefined); 
+// alert(typeof message)
+
+//5.1 object
+
+var person = {
+	"name" : "Nicholas",
+	"age" : 29,
+	5 : true
+};
+
+var now = new Date(Date.parse("May 25, 2001"));
