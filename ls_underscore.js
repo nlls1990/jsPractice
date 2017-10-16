4a23daf1ca020c6919711ea3d54e6da03550581b
nlls1990 <124639013@qq.com>
2017-10-16 12:58:53 +0800

    [js] Add array function initial, last, rest, compact

--- ls_underscore.js
+++ ls_underscore.js
@@ -417,4 +417,20 @@
         return slice.call(array, 0, Math.max(0, array.length - (n == null || guard ? 1 : n)));
     };
 
+    _.last = function(array, n, guard) {
+        if (array == null) return void 0;
+        if (n == null || guard) return array[array.length - 1];
+        return _.rest(array, Math.max(0, array.length - n));
+    };
+
+    _.rest = _.tail = _.drop = function(array, n, guard) {
+        return slice.call(array, n == null || guard ? 1 : n);
+    };
+
+    _.compact = function(array) {
+        return _.filter(array, _.identity);
+    };
+
+    
+
 }.call(this));
