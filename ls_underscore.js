34f445d7211146ead7e1a06a2af5070d6210dc47
nlls1990 <124639013@qq.com>
2017-10-16 13:22:33 +0800

    [js] Add flatten 递归函数

--- ls_underscore.js
+++ ls_underscore.js
@@ -431,6 +431,23 @@
         return _.filter(array, _.identity);
     };
 
-    
+    var flatten = function(input, shalow, strict, startIndex) {
+        var output = [], idx = 0;
+        for (var i = startIndex || 0, length = getLength(input); i < length; i++) {
+            var value = input[i];
+            if (isArrayLike(value) && (_.isArray(value) || _.isArguments(value))) {
+                if (!shallow) value = flatten(value, shallow, strict);
+                var j = 0, len = value.length;
+                output.length += len;
+                while (j < len) {
+                    output[idx++] = value[j++];
+                }
+            } else if (!strict) {
+                output[idx++] = value;
+            }
+        }
+        return output;
+    };
+
 
 }.call(this));
