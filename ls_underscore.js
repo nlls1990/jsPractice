042dc99d02aed5385499628997b35d6ef4499401
nlls1990 <124639013@qq.com>
2017-10-16 16:28:22 +0800

    [js] Add zip and unzip function

--- ls_underscore.js
+++ ls_underscore.js
@@ -510,4 +510,18 @@
         });
     };
 
+    _.zip = function() {
+        return _.unzip(arguments);
+    };
+
+    _.unzip = function(array) {
+        var length = array && _.max(array, getLength).length || 0;
+        var result = Array(length);
+
+        for (var index = 0; index < length; index++) {
+            result[index] = _.pluck(array, index);
+        }
+        return result;
+    };
+
 }.call(this));
