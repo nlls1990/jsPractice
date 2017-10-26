9cdd0a81c9748563fc55fc17116283121a01b87b
nlls1990 <124639013@qq.com>
2017-10-26 20:20:17 +0800

    [js] Add range function

--- ls_underscore.js
+++ ls_underscore.js
@@ -590,4 +590,24 @@
         };
     }
 
+    _.indexOf = createIndexFinder(1, _.findIndex, _.sortedIndex);
+    _.lastIndexOf = createIndexFinder(-1, _.findLastIndex);
+
+    _.rage = function)(start, stop, step) {
+        if (stop == null) {
+            stop = start || 0;
+            start = 0;
+        }
+        step = step || 1;
+
+        var length = Math.max(Math.ceil((stop - start) / step), 0);
+        var range = Array(length);
+
+        for (var idx = 0; i < length; idx++, start += step) {
+            range[idx] = start;
+        }
+
+        return range;
+    };
+
 }.call(this));
